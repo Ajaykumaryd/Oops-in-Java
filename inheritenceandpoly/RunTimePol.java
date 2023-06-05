33ccba370afef74da32680199e682a900f9a459a
Ajay Kumar <127691663+Ajaykumaryd@users.noreply.github.com>
2023-06-05 19:03:56 +0530

    Update RunTimePol.java

--- inheritenceandpoly/RunTimePol.java
+++ inheritenceandpoly/RunTimePol.java
@@ -1,6 +1,10 @@
 package inheritenceandpoly;
 
 public class RunTimePol {
+   
+//Runtime polymorphism, also known as the Dynamic Method Dispatch, is a process that resolves a call to an overridden method at runtime.
+  //The process involves the use of the reference variable of a superclass to call for an overridden method.   
+   
 //    method overiding
 //      we write methods on super and sub classes in Inertience in such a way that method name and parameter must be same called method overiding
 
